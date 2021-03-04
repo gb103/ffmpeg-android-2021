c7c6258f28698cfcf2ee632862f276b4269c5249
Gaurav Bansal <gauravit.321203@gmail.com>
2021-03-04 16:59:18 +0530

    - Adding the FFMPeg library with android 10 (API 29) and android bundle support.

---
+++ FFmpegAndroid/src/main/java/com/github/hiteshsondhi88/libffmpeg/FileUtils.java
@@ -0,0 +1,131 @@
+package com.github.hiteshsondhi88.libffmpeg;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.Map;
+
+class FileUtils {
+
+    static final String ffmpegFileName = "libffmpeg.so";
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+    private static final int EOF = -1;
+
+    static boolean copyBinaryFromAssetsToData(Context context, String fileNameFromAssets, String outputFileName) {
+		
+		// create files directory under /data/data/package name
+		File filesDirectory = getFilesDirectory(context);
+		
+		InputStream is;
+		try {
+			is = context.getAssets().open(fileNameFromAssets);
+			// copy libffmpeg.so.so file from assets to files dir
+			final FileOutputStream os = new FileOutputStream(new File(filesDirectory, outputFileName));
+			byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+			
+			int n;
+			while(EOF != (n = is.read(buffer))) {
+				os.write(buffer, 0, n);
+			}
+
+            Util.close(os);
+            Util.close(is);
+			
+			return true;
+		} catch (IOException e) {
+			Log.e("issue in coping binary from assets to data. ", e);
+		}
+        return false;
+	}
+
+	static File getFilesDirectory(Context context) {
+		// creates files directory under data/data/package name
+        return context.getFilesDir();
+	}
+
+    static String getFFmpeg(Context context) {
+        //return getFilesDirectory(context).getAbsolutePath() + File.separator + FileUtils.ffmpegFileName;
+        String archFolder = "";
+
+        if (CpuArchHelper.getCpuArch() == CpuArch.x86){
+            archFolder = "x86";
+        } else if (CpuArchHelper.getCpuArch() == CpuArch.ARMv7){
+            if(CpuArchHelper.isArm64()) {
+                archFolder = "arm64";
+            } else {
+                archFolder = "arm";
+            }
+        }
+
+        /*Log.v("gb103: " + archFolder);
+
+        String path = context.getPackageResourcePath().replaceAll("/([^/]+)$", "/lib/");
+        File file = new File(path);
+        File[] files = file.listFiles();
+        for(File f: files) {
+            Log.v("gb103: " +  f.getAbsolutePath());
+            File[] files1 = f.listFiles();
+            for(File f1: files1) {
+                Log.v("gb103: insideFile" +  f1.getAbsolutePath());
+            }
+        }
+        Log.v("gb103: searching for - " + context.getPackageResourcePath().replaceAll("/([^/]+)$", "/lib/") + archFolder + "/" + ffmpegFileName);*/
+        return context.getPackageResourcePath().replaceAll("/([^/]+)$", "/lib/") + archFolder + "/" + ffmpegFileName;
+    }
+
+    static String getFFmpeg(Context context, Map<String,String> environmentVars) {
+        String ffmpegCommand = "";
+        if (environmentVars != null) {
+            for (Map.Entry<String, String> var : environmentVars.entrySet()) {
+                ffmpegCommand += var.getKey()+"="+var.getValue()+" ";
+            }
+        }
+        ffmpegCommand += getFFmpeg(context);
+        return ffmpegCommand;
+    }
+
+    static String SHA1(String file) {
+        InputStream is = null;
+        try {
+            is = new BufferedInputStream(new FileInputStream(file));
+            return SHA1(is);
+        } catch (IOException e) {
+            Log.e(e);
+        } finally {
+            Util.close(is);
+        }
+        return null;
+    }
+
+    static String SHA1(InputStream is) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+            final byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+            for (int read; (read = is.read(buffer)) != -1; ) {
+                messageDigest.update(buffer, 0, read);
+            }
+
+            Formatter formatter = new Formatter();
+            // Convert the byte to hex format
+            for (final byte b : messageDigest.digest()) {
+                formatter.format("%02x", b);
+            }
+            return formatter.toString();
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(e);
+        } catch (IOException e) {
+            Log.e(e);
+        } finally {
+            Util.close(is);
+        }
+        return null;
+    }
+}
